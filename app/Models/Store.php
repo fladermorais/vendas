4388f2a2280a77201ed34ab6f9cf158d2151763a
Flademir Morais <fladermorais@hotmail.com>
2026-02-08 17:59:47 -0300

    [Store] Criando model e migration

---
+++ app/Models/Store.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    protected $table = 'stores';
+    protected $fillable = ['name', 'max_users', 'status'];
+}
---
+++ database/migrations/2026_02_03_104154_create_stores_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('Nome da Loja');
+            $table->integer('max_users')->comment('Quantidade máxima de usuários permitidos');
+            $table->enum('status', ['a', 'i'])->default('a')->comment('Status da loja');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stores');
+    }
+};
