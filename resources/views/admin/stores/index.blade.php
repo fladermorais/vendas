197905ac268cc9fd92d0a2f85644d7a9f480f672
Flademir Morais <fladermorais@hotmail.com>
2026-03-08 16:32:31 -0300

    [Store] Ajustando template para uso do adminlte

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,61 +1,67 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between">
-                    <h3>{{ __('Lojas') }}</h3>
-                    <a class="btn btn-sm btn-primary text-center" href="" data-bs-toggle="modal" data-bs-target="#createModal">Novo</a>
-                </div>
-                
-                <div class="card-body">
-                    
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Nome</th>
-                                <th scope="col">Máximo de usuários</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($stores as $store)
-                            <tr>
-                                <th scope="row">{{ $store['id'] }}</th>
-                                <td>{{ $store['name'] }}</td>
-                                <td>{{ $store['max_users'] }}</td>
-                                <td> <span class="badge bg-{{ $store['status'] == "a" ? "success" : "danger" }}">{{ $store['status'] }}</span></td>
-                                <td class="d-flex justify-content-around">
-                                    <a class="btn btn-secondary btn-sm" href="{{ route('store.show', $store['id']) }}"><i class="fa-solid fa-eye"></i></a>
-                                    <a class="btn btn-info btn-sm" href="" data-bs-toggle="modal" data-bs-target="#updateModal_{{ $store['id'] }}"><i class="fa-solid fa-pen-to-square"></i></a>
-                                    <form action="{{ route('store.destroy', $store['id']) }}" method="POST">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button onclick="return(confirm('Deseja realmente excluir este registro?'))" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
+@section('title', 'Lojas')
 
-                                    </form>
-                                </td>
-                            </tr>
-
-                            @include('admin.stores._modalUpdate')
-
-                            @endforeach
-                        </tbody>
-                    </table>
-                    
-                </div>
-            </div>
-        </div>
+@section('content_header')
+<div class="card">
+    <div class="card-header">
+        <h1>Lojas</h1>
     </div>
 </div>
+@stop
 
+@section('content')
+
+<div class="card">
+    
+    <div class="card-header d-flex justify-content-between row">
+        <h3 class="col-md-10">{{ __('Lista de Lojas') }}</h3>
+        <x-adminlte-button label="Novo" data-toggle="modal" data-target="#createModal" class="bg-success col-md-2"/>
+    </div>
+    
+    <div class="card-body">
+        
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Máximo de usuários</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($stores as $store)
+                <tr>
+                    <th scope="row">{{ $store['id'] }}</th>
+                    <td>{{ $store['name'] }}</td>
+                    <td>{{ $store['max_users'] }}</td>
+                    <td> <span class="badge bg-{{ $store['status'] == "a" ? "success" : "danger" }}">{{ $store['status'] }}</span></td>
+                    <td class="d-flex justify-content-around">
+                        <a class="btn btn-secondary btn-sm" href="{{ route('store.show', $store['id']) }}"><i class="fa-solid fa-eye"></i></a>
+                        <x-adminlte-button label="" icon="fa-solid fa-pen-to-square" data-toggle="modal" data-target="#updateModal_{{ $store['id'] }}" class="bg-info btn-sm"/>
+                        {{-- <a class="btn btn-info btn-sm" href="" data-bs-toggle="modal" data-bs-target="#updateModal_{{ $store['id'] }}"><i class="fa-solid fa-pen-to-square"></i></a> --}}
+                        <form action="{{ route('store.destroy', $store['id']) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button onclick="return(confirm('Deseja realmente excluir este registro?'))" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
+                            
+                        </form>
+                    </td>
+                </tr>
+                
+                @include('admin.stores._modalUpdate')
+                
+                @endforeach
+            </tbody>
+        </table>
+        
+    </div>
+</div>
 
 
 @include('admin.stores._modalCreate');
 
 
-@endsection
+@stop
--- resources/views/admin/stores/show.blade.php
+++ resources/views/admin/stores/show.blade.php
@@ -1,64 +1,67 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+<div class="card">
+    <div class="card-header">
+        <h1>Usuários</h1>
+    </div>
+</div>  
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between">
-                    <h3>Dados da Loja: {{ $store->name }}</h3>
-                    <a class="btn btn-sm btn-primary text-center" href="" data-bs-toggle="modal" data-bs-target="#createModalUser">Novo Usuário</a>
-                </div>
+<div class="card">
+    <div class="card-header d-flex justify-content-between row">
+        <h3 class="col-md-10">Usuários da Loja: {{ $store->name }}</h3>
+        <x-adminlte-button label="Novo Usuário" data-toggle="modal" data-target="#createModalUser" class="bg-success col-md-2"/>
+        {{-- <a class="btn btn-sm btn-primary text-center" href="" data-bs-toggle="modal" data-bs-target="#createModalUser">Novo Usuário</a> --}}
+    </div>
+    
+    <div class="card-body">
+        
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            
+            @if(isset($store->users))
+            @php
+            // dd($store->users[0]['id'])
+            @endphp
+            <tbody>
+                @foreach($store->users as $user)
+                <tr>
+                    <th scope="row">{{ $user['id'] }}</th>
+                    <td>{{ $user['name'] }}</td>
+                    <td>{{ $user['email'] }}</td>
+                    <td class="d-flex justify-content-around">
+                        <x-adminlte-button label="" icon="fa-solid fa-pen-to-square" data-toggle="modal" data-target="#updateModalUser_{{ $user['id'] }}" class="bg-info btn-sm col-md-2"/>
+                        <form action="{{ route('user.destroy', $user['id']) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button onclick="return(confirm('Deseja realmente excluir este registro?'))" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
+                            
+                        </form>
+                    </td>
+                </tr>
                 
-                <div class="card-body">
-                    
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Nome</th>
-                                <th scope="col">E-mail</th>
-                                <th scope="col">Ações</th>
-                            </tr>
-                        </thead>
-
-                        @if(isset($store->users))
-                        @php
-                            // dd($store->users[0]['id'])
-                        @endphp
-                        <tbody>
-                            @foreach($store->users as $user)
-                            <tr>
-                                <th scope="row">{{ $user['id'] }}</th>
-                                <td>{{ $user['name'] }}</td>
-                                <td>{{ $user['email'] }}</td>
-                                <td class="d-flex justify-content-around">
-                                    <a class="btn btn-info btn-sm" href="" data-bs-toggle="modal" data-bs-target="#updateModalUser_{{ $user['id'] }}"><i class="fa-solid fa-pen-to-square"></i></a>
-                                    <form action="{{ route('user.destroy', $user['id']) }}" method="POST">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button onclick="return(confirm('Deseja realmente excluir este registro?'))" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
-
-                                    </form>
-                                </td>
-                            </tr>
-
-                            @include('admin.stores._modalUpdateUser')
-
-                            @endforeach
-                        </tbody>
-                        @endif
-                    </table>
-                    
-                </div>
-            </div>
-        </div>
+                @include('admin.stores._modalUpdateUser')
+                
+                @endforeach
+            </tbody>
+            @endif
+        </table>
+        
     </div>
 </div>
 
 
+@include('admin.stores._modalCreateUser')
 
-@include('admin.stores._modalCreateUser');
-
-
-@endsection
+@stop
